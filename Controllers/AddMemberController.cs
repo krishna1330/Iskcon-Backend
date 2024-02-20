d069daea50c4156cd551bee2c286aa5c6204aeab
Krishna Murali <Krishna Murali@DESKTOP-ND33CLR>
2024-02-20 10:20:47 +0530

    Add project files.

---
+++ Controllers/AddMemberController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IskconAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddMemberController : ControllerBase
+    {
+        private readonly string _connectionString;
+        public AddMemberController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SQLConnection") ?? throw new InvalidOperationException("Connection string cannot be null or whitespace");
+        }
+
+
+    }
+}
